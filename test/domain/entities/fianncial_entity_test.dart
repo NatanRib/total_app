495be081f068ca05dacad24d87a14f0f3769d2d0
Natan Ribeiro <ntnmarcal@gmail.com>
2021-12-17 09:52:15 -0300

    test - FianancialEntity - Should caculate totalValue correctly - DONE

---
+++ test/domain/entities/fianncial_entity_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:total_app/features/finance/domain/entities/financial_entity.dart';
+import 'package:total_app/features/finance/domain/enumerated/financial_type_enum.dart';
+
+main(){
+
+  test('Should calculate total value correctly', (){
+    //Arrange
+    FinancialEntity systemUnderTest = FinancialEntity(id: 1, description: "description", value: 120, installments: 2, type: FinancialType.active, paid: false);
+    
+    //Assert
+    expect(systemUnderTest.totalValue, 240.0); 
+  });
+}
